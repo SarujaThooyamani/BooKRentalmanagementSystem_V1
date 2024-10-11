9440c21c09190cb3ac15cda0f26f2e961d7f6ccb
ut01372 <ut01372@UNICOMTIC6>
2024-10-11 14:12:15 +0530

    Add project files.

---
+++ BooKRentalmanagementSystem_V1/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooKRentalmanagementSystem_V1
+{
+    internal class Book
+    {
+        private string Bookid;
+        private string Title;
+        private string Author;
+        private decimal RentalPrice;
+
+        public Book(string bookid, string title, string author, decimal rentalPrice)
+        {
+            Bookid = bookid;
+            Title = title;
+            Author = author;
+            RentalPrice = rentalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"Bookid: {Bookid},Title: {Title},Author:{Author},RentalPrice:{RentalPrice}";
+        }
+        public Book book1 = new Book();
+    }
+}
---
+++ BooKRentalmanagementSystem_V1/BookManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooKRentalmanagementSystem_V1
+{
+    internal class BookManager
+    {
+        public List<Book> books=new List<Book>();
+        public void CreateBook(Book book)
+        {
+
+            books.Add(book);
+            Console.WriteLine("Book added Successfully");
+        }
+
+        public void ReadBooks()
+        {
+            if(books.Count != 0)
+            {
+                foreach (Book book in books)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+            else
+            {
+                Console.WriteLine("no books to show");
+            }
+        }
+        public void UpdateBook(Book book)
+        {
+            var data =books.Find(b=>b.BookId==book)
+        }
+
+    }
+
+    
+}
