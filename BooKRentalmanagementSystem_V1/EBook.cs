3885b4408e3dfe6ea5075f4694c70a21e683d3c5
UT01372 <zarasaru07@gmail.com>
2024-10-11 15:04:56 +0530

    6th qs (a)

---
+++ BooKRentalmanagementSystem_V1/EBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooKRentalmanagementSystem_V1
+{
+    internal class EBook:Book
+    {
+        public double Filesize;
+        public string Format;
+
+        public EBook(string bookid, string title, string author, decimal rentalPrice , double filesize,string format) : base(bookid, title, author, rentalPrice)
+        {
+            this.Format = format;
+            this.Filesize = filesize;
+        }
+    }
+}
---
+++ BooKRentalmanagementSystem_V1/PrintedBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooKRentalmanagementSystem_V1
+{
+    internal class PrintedBook : Book
+    {
+
+        public int ISBN;
+        public int NumberOfPages;
+
+        public PrintedBook(string bookid, string title, string author, decimal rentalPrice,int isbn,int numberofpage) : base(bookid, title, author, rentalPrice)
+        {
+            ISBN=isbn;
+            NumberOfPages=numberofpage;
+        }
+    }
+}
